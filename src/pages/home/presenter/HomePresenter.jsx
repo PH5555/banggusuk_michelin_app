7c2f474d20311b4f88a59efe1d3ec18a989356fb
김동현 <lmkn5342@naver.com>
2024-12-01 14:07:48 +0000

    : add map in main page

--- src/pages/home/presenter/HomePresenter.jsx
+++ src/pages/home/presenter/HomePresenter.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Map } from "react-kakao-maps-sdk"
 
 const HomePresenter = () => {
-	return <div></div>
+	return (
+		<Map
+		  center={{ lat: 33.5563, lng: 126.79581 }}
+		  style={{ width: "100%", height: "100vh" }}
+		>
+		 
+    	</Map>
+	)
 }
 
 export default HomePresenter;
