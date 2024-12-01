fa28a13dc1d002e92d34a353e07a4458d40df97c
김동현 <lmkn5342@naver.com>
2024-12-01 05:38:15 +0000

    `design` : gruop page design

--- src/pages/group/presenter/GroupPresenter.jsx
+++ src/pages/group/presenter/GroupPresenter.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import * as Style from './GroupPresenter.style';
+import { Button } from '../../../components/button/Button';
 
 const GroupPresenter = () => {
-	return <div></div>
+	return (
+		<Style.Container>
+			<img src={require('../../../assets/images/logo.png')} width={107} height={107}/>
+			<Style.Text size={20} top={27}>우리들만의</Style.Text>
+			<Style.Text size={16} bottom={86}>미슐랭 가이드 만들기</Style.Text>
+			<Button backgroundColor={'#FF4F26'} color={'#ffffff'}>그룹 만들기</Button>
+			<Style.Box/>
+			<Button backgroundColor={'#ffffff'} color={'#FF4F26'} shadow={true}>그룹에 가입하기</Button>
+		</Style.Container>
+	)
 }
 
 export default GroupPresenter;
--- src/pages/login/presenter/LoginPresenter.jsx
+++ src/pages/login/presenter/LoginPresenter.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Button } from '../../../components/button/Button';
 import * as Style from './LoginPresenter.style';
-import {logo} from '../../../assets/images/logo.png';
 
 const LoginPresenter = () => {
 	return (
-	<Style.Container>
-		<img src={require('../../../assets/images/logo.png')} width={107} height={107}/>
-		<Style.Text size={20} top={27}>우리들만의</Style.Text>
-		<Style.Text size={16} bottom={86}>미슐랭 가이드 만들기</Style.Text>
-		<Button backgroundColor={'#FEE500'} color={'#000000'}>카카오로 로그인</Button>
-	</Style.Container>
+		<Style.Container>
+			<img src={require('../../../assets/images/logo.png')} width={107} height={107}/>
+			<Style.Text size={20} top={27}>우리들만의</Style.Text>
+			<Style.Text size={16} bottom={86}>미슐랭 가이드 만들기</Style.Text>
+			<Button backgroundColor={'#FEE500'} color={'#000000'}>카카오로 로그인</Button>
+		</Style.Container>
 	)
 }
 
