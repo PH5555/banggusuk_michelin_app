ed191b0892c7490c99b4e7bf575bdc1580f5a8f7
김동현 <lmkn5342@naver.com>
2024-12-01 05:06:01 +0000

    `design` : design login page

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled, { css } from "styled-components";
+
+export const Button = ({backgroundColor, color, shadow, children}) => {
+  return (
+    <Container backgroundColor={backgroundColor} color={color} shadow={shadow}>{children}</Container>
+  )
+}
+
+const Container = styled.div`
+    width: 315px;
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 16px;
+	justify-content: center;
+    color: ${(props) => props.color};
+    background-color: ${(props) => props.backgroundColor};
+    box-shadow: ${(props) => props.shadow ? '4px 4px 23px 2px rgba(0, 0, 0, 0.25)' : ''};
+    border-radius: 10px;
+`
--- src/pages/login/presenter/LoginPresenter.jsx
+++ src/pages/login/presenter/LoginPresenter.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Button } from '../../../components/button/Button';
+import * as Style from './LoginPresenter.style';
+import {logo} from '../../../assets/images/logo.png';
 
 const LoginPresenter = () => {
-	return <div>hi</div>
+	return (
+	<Style.Container>
+		<img src={require('../../../assets/images/logo.png')} width={107} height={107}/>
+		<Style.Text size={20} top={27}>우리들만의</Style.Text>
+		<Style.Text size={16} bottom={86}>미슐랭 가이드 만들기</Style.Text>
+		<Button backgroundColor={'#FEE500'} color={'#000000'}>카카오로 로그인</Button>
+	</Style.Container>
+	)
 }
 
 export default LoginPresenter;
